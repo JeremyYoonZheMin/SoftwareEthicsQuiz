fa35e6b39ebff4cd128a6ab8549233842c50fa9d
ShenyiHu <shuu0020@student.monash.edu>
2023-05-15 04:50:34 +1000

    Added Landing page and Terms&Conditions page.

--- software_ethics_quiz/src/App.js
+++ software_ethics_quiz/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from "./pages/landing";
+import Terms from "./pages/terms";
 import Questions from '../src/pages/questions';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <main className="App">
-      <Router>
-          <Routes>
-            <Route path='/questions' exact element={<Questions/>} />
-          </Routes>
-        </Router>
-    </main>
-  );
+    return (
+        <main className="App">
+            <Router>
+                <Routes>
+                    <Route path='' exact element={<Landing/>}/>
+                    <Route path='/terms' exact element={<Terms/>}/>
+                    <Route path='/questions' exact element={<Questions/>}/>
+                </Routes>
+            </Router>
+        </main>
+    );
 }
 
 export default App;
---
+++ software_ethics_quiz/src/pages/landing.js
@@ -0,0 +1,42 @@
+import '../styles/landing.css'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import {useNavigate} from "react-router-dom";
+
+export default function Landing() {
+    const navigate = useNavigate();
+    function routeChange() {
+        const path = "/terms";
+        navigate(path);
+    }
+
+    return (
+            <div className="landingPage">
+                <Container fluid className='contentContainer'>
+                    <Row>
+                        <div className='landingTitle'>
+                            Software Ethics Quiz
+                        </div>
+                    </Row>
+                    <Row>
+                        <div className='landingSubtitle'>
+                            How Ethical Are You?
+                        </div>
+                    </Row>
+                    <Row>
+                        <div className='startButtonContainer'>
+                            <Button
+                                variant='outline-light'
+                                size='lg'
+                                className='startButton'
+                                onClick={routeChange}
+                            >
+                                Start
+                            </Button>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+        )
+}
