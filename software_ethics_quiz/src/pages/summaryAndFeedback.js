57923f2897168e0808186212ab0f38b7c5cbc076
Tian Ru Er <oohpier@gmail.com>
2023-05-15 22:10:36 +1000

    added icons

--- software_ethics_quiz/src/pages/summaryAndFeedback.js
+++ software_ethics_quiz/src/pages/summaryAndFeedback.js
@@ -1,40 +1,45 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Button, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Button, ListGroup, Stack } from 'react-bootstrap';
 import '../styles/summaryAndFeedback.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function SummaryAndFeedback() {
-    const [currentQuestion, setCurrentQuestion] = useState(1);
+    const [feedbackBoxTitle, setFeedbackBoxTitle] = useState("Good Try!");
+    const [feedbackBoxContent, setFeedbackBoxContent] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem cursus, convallis ex in, rutrum lorem. Curabitur efficitur ante ac congue sodales. Mauris varius ac sapien sit amet fermentum. Morbi quis dui efficitur mauris ultrices ullamcorper in quis nibh. Nulla dignissim eu ex at imperdiet. Maecenas sollicitudin venenatis ex ut porttitor. Vestibulum eget sodales sapien, sed vehicula orci. Nunc vel augue vitae orci vestibulum molestie. Sed dapibus urna sed facilisis sagittis.");
 
     const handleClick = (questionNumber) => {
-        setCurrentQuestion(questionNumber);
+        setFeedbackBoxTitle("Question " + questionNumber);
+        setFeedbackBoxContent("content of " + questionNumber);
     };
 
     return (
         <div className="summaryAndFeedbackPage">
-            
             <div className="summaryAndFeedbackHeader">
                 <h1 className='headerTitle'>Summary and Feedback</h1>
             </div>
-            <Container>
-                <Row>
-                    <Col>1 of 2</Col>
-                    <Col>2 of 2</Col>
-                </Row>
-                <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
-                </Row>
-            </Container>
             <div className='summaryAndFeedbackContent'>
 
                 <Container>
-                    <Row className="content">
-                        <Col sm={8} className="feedback-container">
-                            <div className="feedback">
-                                <h2>Question {currentQuestion}</h2>
-                                <p>Content for Question {currentQuestion} goes here.</p>
-                            </div>
+                    <Row>
+                        <Col sm={8} className="feedback-container ">
+                            <Stack gap={3} justify-content-center>
+                                <div>
+                                    <h2 className='feedbackBoxTitle'>{feedbackBoxTitle}</h2>
+                                    <div className="feedbackBox ">
+                                        <FontAwesomeIcon icon={faStar} id='star1' className='star' />
+                                        <FontAwesomeIcon icon={faStar} id='star2' className='star' />
+                                        <FontAwesomeIcon icon={faStar} id='star3' className='star' />
+                                        <FontAwesomeIcon icon={faStar} id='star4' className='star' />
+                                        <FontAwesomeIcon icon={faStar} id='star5' className='star' />
+                                        <p>
+                                            {feedbackBoxContent}
+                                        </p>
+                                    </div>
+                                </div>
+                                <button type="button" class="startButton btn btn-outline-light btn-sm" id='scenarioBtn'>Scenario</button>
+                            </Stack>
+
                         </Col>
                         <Col sm={4} className="questions-container">
                             <ListGroup class="list-group-item">
