660b5ea55bac4d1ac753f868aaf5f01c569fb9ae
Tian Ru Er <oohpier@gmail.com>
2023-08-06 14:43:30 +1000

    coloured stars based on score

--- software_ethics_quiz/src/pages/summaryAndFeedback.js
+++ software_ethics_quiz/src/pages/summaryAndFeedback.js
@@ -3,15 +3,65 @@ import { Container, Row, Col, Button, Stack } from 'react-bootstrap';
 import '../styles/summaryAndFeedback.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { scenariosAndQuestions } from '../constants/scenariosAndQuestions';
 
 function SummaryAndFeedback() {
-    const [feedbackBoxTitle, setFeedbackBoxTitle] = useState("Good Try!");
-    const [feedbackBoxContent, setFeedbackBoxContent] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem cursus, convallis ex in, rutrum lorem. Curabitur efficitur ante ac congue sodales. Mauris varius ac sapien sit amet fermentum. Morbi quis dui efficitur mauris ultrices ullamcorper in quis nibh. Nulla dignissim eu ex at imperdiet. Maecenas sollicitudin venenatis ex ut porttitor. Vestibulum eget sodales sapien, sed vehicula orci. Nunc vel augue vitae orci vestibulum molestie. Sed dapibus urna sed facilisis sagittis.");
+    // TODO: get answers from other pg
+    let selectedAnswers = [
+        [3, 2, 3, 3],
+        [3, 3, 3, 3],
+        [3, 3, 3, 3],
+        [3, 3, 3, 3]
+    ];
+    let maxScore = 16 * 4;
+
+
+    const getScore = (selectedAnswers, maxScore) => {
+        let score = 0;
+        selectedAnswers.forEach(function (scenarioAns, scenario) {
+            scenarioAns.forEach(function (ans, ques) {
+                score += scenariosAndQuestions[scenario].questions[ques].answers[ans].score;
+            });
+        });
+        return Math.round(score / maxScore);
+        // return score;
+    };
+
+    const getStars = (score) => {
+        const stars = [];
+        let numStars = 5;
+        for (let i = 0; i < score; i++) {
+            stars.push(<FontAwesomeIcon icon={faStar} className='star' color='orange' />);
+        }
+        for (let i = 0; i < (numStars - score); i++) {
+            stars.push(<FontAwesomeIcon icon={faStar} className='star' />);
+        }
+
+        return stars;
+    };
 
     const handleClick = (questionNumber) => {
+        let numScenarios = 4;
+        let totalQuestions = 16;
+        let scenario = Math.ceil(questionNumber/numScenarios);
+        let question = totalQuestions - questionNumber;
+
+        q = qByScenario * scenario
+
         setFeedbackBoxTitle("Question " + questionNumber);
-        setFeedbackBoxContent("content of " + questionNumber);
+        // setFeedbackBoxContent("content of " + questionNumber);
+        setFeedbackBoxContent(scenariosAndQuestions[scenario].questions[ques].answers[ans].score);
     };
+
+
+    let score = getScore(selectedAnswers, maxScore);
+
+
+    const [feedbackBoxTitle, setFeedbackBoxTitle] = useState("Good Try!");
+    // const [feedbackBoxContent, setFeedbackBoxContent] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in sem cursus, convallis ex in, rutrum lorem. Curabitur efficitur ante ac congue sodales. Mauris varius ac sapien sit amet fermentum. Morbi quis dui efficitur mauris ultrices ullamcorper in quis nibh. Nulla dignissim eu ex at imperdiet. Maecenas sollicitudin venenatis ex ut porttitor. Vestibulum eget sodales sapien, sed vehicula orci. Nunc vel augue vitae orci vestibulum molestie. Sed dapibus urna sed facilisis sagittis.");
+    const stars = useState(getStars(score));
+    const [feedbackBoxContent, setFeedbackBoxContent] = useState(score);
+
 
     return (
         <div className="summaryAndFeedbackPage">
@@ -27,25 +77,21 @@ function SummaryAndFeedback() {
                                 <div>
                                     <h2 className='feedbackBoxTitle'>{feedbackBoxTitle}</h2>
                                     <div className="feedbackBox ">
-                                        <FontAwesomeIcon icon={faStar} id='star1' className='star' />
-                                        <FontAwesomeIcon icon={faStar} id='star2' className='star' />
-                                        <FontAwesomeIcon icon={faStar} id='star3' className='star' />
-                                        <FontAwesomeIcon icon={faStar} id='star4' className='star' />
-                                        <FontAwesomeIcon icon={faStar} id='star5' className='star' />
+                                        {stars}
                                         <p>
                                             {feedbackBoxContent}
                                         </p>
                                     </div>
                                 </div>
-                                <button type="button" class="startButton btn btn-outline-light btn-sm" id='scenarioBtn'>Scenario</button>
+                                <button type="button" class="startButton btn btn-outline-light btn-sm" id='scenarioBtn' hidden>Scenario</button>
                             </Stack>
 
                         </Col>
-                        <Col sm={4} className="questions-container">                          
+                        <Col sm={4} className="questions-container">
                             <Stack gap={1}>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(1)}>Question 1</Button>
-                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(2)}>Question 2</Button>                               
-                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(3)}>Question 3</Button>                               
+                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(2)}>Question 2</Button>
+                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(3)}>Question 3</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(4)}>Question 4</Button>
                             </Stack>
                             <br />
@@ -59,21 +105,21 @@ function SummaryAndFeedback() {
                             <Stack gap={1}>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(9)}>Question 9</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(10)}>Question 10</Button>
-                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(11)}>Question 11</Button>  
+                                <Button variant='outline-light' className="questionButton" onClick={() => handleClick(11)}>Question 11</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(12)}>Question 12</Button>
-                            </Stack>    
+                            </Stack>
                             <br />
                             <Stack gap={1}>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(13)}>Question 13</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(14)}>Question 14</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(15)}>Question 15</Button>
                                 <Button variant='outline-light' className="questionButton" onClick={() => handleClick(16)}>Question 16</Button>
-                            </Stack>                        
+                            </Stack>
                         </Col>
                     </Row>
                     <Row>
                         <Col>
-                        <Button variant='outline-light' className="tryAgainButton">Try Again</Button>
+                            <Button variant='outline-light' className="tryAgainButton">Try Again</Button>
                         </Col>
                     </Row>
                 </Container>
