374a797cfb46794baaeba6567659329d4aea65eb
Nabeeb Yusuf <nabeeb2000@gmail.com>
2023-08-28 22:46:10 +1000

    adjusted object structure for db

--- server/routes/answer.mjs
+++ server/routes/answer.mjs
@@ -6,14 +6,14 @@ const router = express.Router();
 
 // This section will help you get a list of all the answers.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("answers");
+  let collection = await db.collection("responses");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
 // This section will help you get a single answer by id
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("answers");
+  let collection = await db.collection("responses");
   let query = {_id: new ObjectId(req.params.id)};
   let result = await collection.findOne(query);
 
@@ -24,9 +24,10 @@ router.get("/:id", async (req, res) => {
 // This section will help you create a new answer.
 router.post("/", async (req, res) => {
   let newDocument = {
-    answers: req.body,
+    profession: req.body.profession,
+    answers: req.body.answers
   };
-  let collection = await db.collection("answers");
+  let collection = await db.collection("responses");
   let result = await collection.insertOne(newDocument);
   res.send(result).status(204);
 });
@@ -35,7 +36,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
 
-  const collection = db.collection("answers");
+  const collection = db.collection("responses");
   let result = await collection.deleteOne(query);
 
   res.send(result).status(200);
